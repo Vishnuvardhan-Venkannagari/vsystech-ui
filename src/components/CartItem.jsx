f92999c4736c86ea574a1c5f03380abcdb1e23c7
Vishnu Reddy <vishnureddy@Vishnus-Air.hsd1.il.comcast.net>
2024-11-06 21:30:08 -0600

    cahnges

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,5 @@
 import React, {useState} from 'react'
 import "./CartItem.css"
-import Button from "./Button.jsx"
-import { useForm } from "react-hook-form"
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import cartService from '../apiInterface/cart.js'
-import { useSelector } from "react-redux"
 
 
 export default function CartItem({item}) {
@@ -15,7 +9,7 @@ export default function CartItem({item}) {
       <div className="cart-details">
         <h1>{item.productData.name}</h1>
         <h3>Price: ${item.productData.price}</h3>
-        <h2>Status: {item.status}</h2>
+        <h2>Status: In Cart</h2>
       </div>
     </div>
   )
--- src/components/UserCart.jsx
+++ src/components/UserCart.jsx
@@ -4,19 +4,31 @@ import cartService from '../apiInterface/cart.js'
 import Container from "./container/Container"
 import CartItem from "./CartItem.jsx"
 import "./UserCart.css"
+import Button from "./Button.jsx"
 
 export default function UserCart() {
   const authtoken = useSelector((state) => state.auth.authtoken)
   const authStatus = useSelector((state) => state.auth.status)
   const [cartItems, setCartItems] = useState([])
   const [loading, setLoading] = useState(true);
+  
+  const [totalPrice, setTotalPrice] = useState(0)
+  const [totalItems, setTotalItems] = useState(0)
   useEffect(() => {
     const fetchCart = async() => {
       try {
         setLoading(false)
         const cartResponse = await cartService.getUserCart(authtoken)
         // console.log(cartResponse)
-        if (cartResponse) setCartItems(cartResponse)
+        if (cartResponse) {
+          setCartItems(cartResponse)
+          setTotalItems(cartResponse.length)
+          let addPrice = 0
+          cartResponse.map((item) => {
+            addPrice = addPrice + item.productData.price
+          })
+          setTotalPrice(addPrice)
+        }
       } catch (error) {
         console.error("Error fetching carts:", error.message) 
       }
@@ -33,12 +45,21 @@ export default function UserCart() {
         <img src="../../loading.png" alt="Loading..." /> // Loading indicator
       ) : cartItems.length > 0 ? (
         <Container>
-          <div className="w-1/2">
+          <div className="w-full grid grid-cols-2 gap-2">
             {cartItems.map((item) => (
               <div className="p-2 w-full" key={item.id}>
                 <CartItem item={item} />
               </div>
             ))}
+          </div>
+          <div className='price-container'>
+            <h1>Total Items Selected: </h1>
+            <h2>{totalItems}</h2>
+            <h1>Total Price: </h1>
+            <h2>{totalPrice}</h2>
+            <Button type="submit" >
+              Proceed to Buy
+            </Button>
           </div>
         </Container>
       ) : (
